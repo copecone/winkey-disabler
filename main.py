e59a39b16468164a8a60597bf01e1611fc51369e
컨트롤D <controld3785@gmail.com>
2022-03-27 21:53:37 +0900

    스크립트 재수정

--- main.py
+++ main.py
@@ -2,32 +2,50 @@ import keyboard, time
 
 win_enabled = False
 win_handler = None
+alt_handler = None
+win_toggled = False
 win_timestamp = 0
 
 def win_toggle(_str):
     global win_enabled
-    win_enabled = not win_enabled
-    if win_enabled: win_disable()
-    else: win_enable()
+    if not win_toggled:
+        win_enabled = not win_enabled
+        if win_enabled: win_disable()
+        else: win_enable()
 
 def win_callback(_var):
-    global win_timestamp
-    if keyboard.is_pressed('alt') and time.time() - win_timestamp >= 0.1:
+    global win_timestamp, win_toggled
+    if keyboard.is_pressed('alt'):
         win_toggle("")
 
+def alt_release(_str):
+    global win_toggled
+    win_toggled = False
+    keyboard.unhook(alt_handler)
+
 def win_disable():
-    global win_handler, win_timestamp
+    global win_handler, alt_handler, win_timestamp, win_toggled
+
+    win_toggled = True
     win_timestamp = time.time()
     win_handler = keyboard.hook_key("windows", win_callback, suppress=True)
+    alt_handler = keyboard.on_release_key("alt", alt_release, suppress=False)
+
     if keyboard.is_pressed('windows'):
         keyboard.release("windows")
     
     print("Win Key Disabled")
 
 def win_enable():
-    global running
+    global running, win_toggled, alt_handler
+    win_toggled = True
     keyboard.unhook(win_handler)
+    alt_handler = keyboard.on_release_key("alt", alt_release, suppress=False)
     print("Win Key Enabled")
 
 keyboard.add_hotkey("alt+windows", win_toggle, args=("", ))
-keyboard.wait()
+
+try:
+    keyboard.wait()
+except KeyboardInterrupt:
+    pass
