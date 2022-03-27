55e9e3e0d58c74b74908c13fd0f9e7547e7b5c09
컨트롤D <controld3785@gmail.com>
2022-03-27 21:40:49 +0900

    스크립트 수정

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-import keyboard
+import keyboard, time
 
 win_enabled = False
 win_handler = None
-def win_toggle():
+win_timestamp = 0
+
+def win_toggle(_str):
     global win_enabled
     win_enabled = not win_enabled
     if win_enabled: win_disable()
     else: win_enable()
 
 def win_callback(_var):
-    if keyboard.is_pressed('alt'):
-        win_toggle()
+    global win_timestamp
+    if keyboard.is_pressed('alt') and time.time() - win_timestamp >= 0.1:
+        win_toggle("")
 
 def win_disable():
-    global win_handler
+    global win_handler, win_timestamp
+    win_timestamp = time.time()
     win_handler = keyboard.hook_key("windows", win_callback, suppress=True)
+    if keyboard.is_pressed('windows'):
+        keyboard.release("windows")
+    
     print("Win Key Disabled")
 
 def win_enable():
